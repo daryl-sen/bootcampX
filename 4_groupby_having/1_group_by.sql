b037303d5469e3268cfe3107a0494407a68a2ad5
daryl <tsdaryl@gmail.com>
2021-02-01 16:07:45 -0800

    complete 4_groupby_having

---
+++ 4_groupby_having/1_group_by.sql
@@ -0,0 +1,15 @@
+-- wrong answer
+-- SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE (students.end_date IS NULL)
+-- AND (students.total_submissions < 100)
+-- GROUP BY students.name;
+
+-- -- answer
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING count(assignment_submissions.*) < 100;
