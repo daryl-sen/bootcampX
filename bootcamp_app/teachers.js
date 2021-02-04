fc31038dda5bf4b803d02beb20dcb1c52ef35b4d
daryl <tsdaryl@gmail.com>
2021-02-03 17:23:06 -0800

    work on teachers.js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const args = process.argv.slice(2);
+
+const cohortName = args[0];
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${cohortName}'
+ORDER BY teacher;
+`)
+.then(res => {
+  console.log(res.rows);
+})
+.catch(err => console.error('query error', err.stack));
